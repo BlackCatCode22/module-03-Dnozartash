0afcef41971ee0402258488d11a64291417d1658
Dnozartash <155946733+Dnozartash@users.noreply.github.com>
2024-02-09 21:52:46 -0800

    Add files via upload

--- App.java
+++ App.java
@@ -1,21 +1,18 @@
-package mystudent;
-
 public class App {
     public static void main(String[] args) {
-        System.out.println("\n Hello from class App \n\n");
 
-        // Create a student object
-        Student myStudent = new Student();
+    Student myStudent = new Student();
+    myStudent.firstName = "Dariush (DJ)";
+    myStudent.lastName = "Nozartash";
+    myStudent.major = "Computer Science";
+    myStudent.gpa = 3.5;
+    myStudent.age = 31;
+    myStudent.city = "Fresno";
+    myStudent.onProbation = false;
 
-        // Fill the new myStudent object's data fields
-        myStudent.firstName = "Dennis";
-        myStudent.lastname = "Mohle";
-        myStudent.gpa = 3.4;
-        myStudent.major = "Computer Science";
-        myStudent.city = "Fresno";
+    System.out.println(myStudent.firstName);
 
-        System.out.println("\n The first name of the student is: " + myStudent.firstName + "\n");
-        System.out.println("\n The last name of the student is: " + myStudent.lastname + "\n");
 
     }
+
 }
